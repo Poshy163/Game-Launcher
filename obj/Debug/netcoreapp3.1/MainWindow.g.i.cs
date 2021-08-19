20676a1f0ff0e85f0e1d9ec964e53af9e754adba
Joshua Leaper <leaperj@students.kbgs.sa.edu.au>
2021-08-19 12:35:37 +0930

    All around fixes for 1.1.5 beta release

--- obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -85,7 +85,7 @@ namespace GameLauncher {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Game Launcher;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Game Launcher;V1.1.5.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
